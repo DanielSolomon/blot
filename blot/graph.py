9ad45938e1f7a1fea0ca6041a0ac4be1abb1fbac
Daniel <danielsolomon94.ds@gmail.com>
2018-06-16 20:37:50 +0300

    Added CR.

--- blot/graph.py
+++ blot/graph.py
@@ -2,8 +2,14 @@
 Graph module that contains the Graph class and the Point namedtuple.
 Provides utilities to save and format graph information.
 """
+################################################################################################
+# CR (Daniel): CREATE `test_graph.py` SCRIPT IN TESTS DIRECTORY AND TEST YOUR CODE, YOU SILLY. #
+################################################################################################
+
+# CR (Daniel): Dude don't use from ... import ..., confusing as fuck (use from ... import ... only when you import something inside the package).
 from collections import namedtuple
 
+# CR (Daniel): Consider Point class, so we can easily get the quarter it points.
 Point = namedtuple('Point', ['x', 'y'])
 
 
@@ -34,9 +40,11 @@ class Graph:
         self.negative_x = negative_x
         self.negative_y = negative_y
         self.width = (length_x*2 if negative_x else length_x) + 1
+        # CR (Daniel): height?
         self.length = (length_y*2 if negative_y else length_y) + 1
         self.graph = [[' ']*(self.width) for _ in range(self.length)]
         self[(0, 0)] = '0'
+        # CR (Daniel): Make some space between self assignments and rest of the code.
         for i in range(self.length_x):
             self[(i+1, 0)] = str(int(self.step_x * (i+1)) % self.normalizer_x)
             if self.negative_x:
@@ -47,7 +55,9 @@ class Graph:
                 self[(0, -(i+1))] = str(int(self.step_y * (i+1)) % self.normalizer_y)
 
     def __getitem__(self, point: Point) -> str:
+        # CR (Daniel): Why?
         point = Point(*point)
+        # CR (Daniel): Why? Try to index it, if it fails, it will raise IndexError...
         if (point.x < 0 and not self.negative_x) or (point.y < 0 and not self.negative_y):
             raise IndexError("Illegal negative index value supplied!")
         if not -self.length_x <= point.x <= self.length_x or not -self.length_y <= point.y <= self.length_y:
@@ -58,6 +68,7 @@ class Graph:
 
     def __setitem__(self, point: Point, char: str) -> None:
         point = Point(*point)
+        # CR (Daniel): Same.
         if (point.x < 0 and not self.negative_x) or (point.y < 0 and not self.negative_y):
             raise IndexError("Illegal negative index value supplied!")
         if not -self.length_x <= point.x <= self.length_x or not -self.length_y <= point.y <= self.length_y:
@@ -65,6 +76,8 @@ class Graph:
         true_x = point.x + int(self.width / 2) if self.negative_x else point.x
         true_y = point.y + int(self.length / 2) if self.negative_y else point.y
         self.graph[true_y][true_x] = char
+
+    # CR (Daniel): Write _draw function which returns the list represents the graph, then draw will only call it, join it and print it (will be useful for tests).
 
     def draw(self) -> None:
         """
